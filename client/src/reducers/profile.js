c319ddaa3a7cc8ffe6439ec7f1555ab7dd7e7657
Steven De Avila <stevejd88@gmail.com>
2020-04-13 12:17:00 -0500

    uodated PROFILE_ERROR reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -4,7 +4,7 @@ import {
   GET_REPOS,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE,
+  UPDATE_PROFILE
 } from "../actions/types";
 
 const initialState = {
@@ -12,7 +12,7 @@ const initialState = {
   profiles: [],
   repos: [],
   loading: true,
-  error: {},
+  error: {}
 };
 
 export default function (state = initialState, action) {
@@ -24,32 +24,33 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
-        loading: false,
+        loading: false
       };
     case GET_PROFILES:
       return {
         ...state,
         profiles: payload,
-        loading: false,
+        loading: false
       };
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
+        profile: null
       };
     case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
         repos: [],
-        loading: false,
+        loading: false
       };
     case GET_REPOS:
       return {
         ...state,
         repos: payload,
-        loading: false,
+        loading: false
       };
     default:
       return state;
